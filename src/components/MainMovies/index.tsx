be3088d46c3b16c60eb0bd8fcc02beb0cf5a87e0
Gabriel Silveira <ramos97033@outlook.com>
2024-08-27 16:29:49 -0300

    restart

---
+++ src/components/MainMovies/index.tsx
@@ -0,0 +1,18 @@
+const MainMovies = () => {
+    return(
+        <main className="grid grid-cols-3 ">
+            <section className="col-span-2">Main movie</section>
+            <section className="col-span-1">
+                Destaques
+                <ul>
+                    <li>filmes destaque</li>
+                    <li>filmes destaque</li>
+                    <li>filmes destaque</li>
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+
+export default MainMovies
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import { IoMdOptions } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
+
+
+
+const NavBar = () => {
+    return(
+        <section className="flex items-center px-4 py-2">
+        <Image 
+                alt="Logo"
+                src={'/Logo.svg'}
+                width={128}
+                height={34.88}
+        />
+        <div className="navbar flex justify-center gap-3">
+            <section className="input-section rounded-full flex gap-2 p-1">
+                <button className="search-button text-xl rounded-full p-2 duration-200"><IoSearch/></button>
+                <input type="text" placeholder="Pesquisar..." className="input-css rounded-full " />
+            </section>
+            <button className="filter-button text-xl rounded-2xl p-3 duration-200 "><IoMdOptions/></button>
+        </div>
+        </section>
+    )
+}
+
+
+export default NavBar
